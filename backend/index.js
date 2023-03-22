4f47dd14795c87145baaa259ecc0601eb4b982d7
dylancdavis <dylcdav@gmail.com>
2023-03-22 11:31:59 -0400

    Add .gitignore to backend

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,19 @@
-console.log("hello world");
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+const PORT = 3001;
+
+const testData = {
+  propRed: "red",
+  propBlue: "blue",
+};
+
+app.use(morgan("tiny"));
+
+app.get("/", (req, res) => {
+  res.json(testData);
+});
+
+app.listen(PORT, () => {
+  `Server running on port ${PORT}`;
+});

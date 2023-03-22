fe6012ff92bca5cb10ddec6f39d135877440278d
dylancdavis <dylcdav@gmail.com>
2023-03-22 15:39:47 -0400

    Remove geonames API logic

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,16 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Banner from "./components/Banner";
-import geonamesService from "./services/geonamesService";
 import { TextField, Autocomplete } from "@mui/material";
 
 function App() {
-  const [states, setStates] = useState([]);
-
-  useEffect(() => {
-    (async () => {
-      const data = await geonamesService.getStates();
-      setStates(data.geonames);
-    })();
-  }, []);
+  const [states, setStates] = useState(["New York", "California"]);
 
   return (
     <div className="App">
       <Banner />
       {console.log("states", states)}
       <Autocomplete
-        options={states.map((s) => s.name)}
+        options={states}
         sx={{ width: 300 }}
         renderInput={(params) => <TextField {...params} label="State" />}
       />

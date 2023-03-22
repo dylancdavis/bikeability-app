7c585616c4971676184546833773bfc9656a55ec
Cole D <dylcdav@gmail.com>
2023-03-22 12:36:17 -0400

    Add Banner component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import {useState, useEffect} from 'react'
+import Banner from './components/Banner'
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
   
   return (
   <div className="App">
+    <Banner />
     {cities.map(c => {
       return (<div key={c.name}>{c.name}</div>)
     })}
